5de1280f07ed9c75cade057d408c72e8f342f08b
rasyidtanjung <rasyidtanjung21@gmail.com>
2024-02-07 20:59:33 +0700

    penambahan rumus lingkaran

---
+++ segitiga.js
@@ -0,0 +1,17 @@
+// Jangan sentuh code disini
+function TentukanLuasDanKelilingSegitigaSamaSisi(a, t) {
+    const a = a;
+    const t = t;
+    // TODO: buat logic dibawah sini dimana alasnya 1 cm dan tingginya 2 cm
+    console.log("Alas Segitiga:", alas, "cm");
+    console.log("Tingga Segitiga:", tinggi, "cm");
+    const l = (alas * tinggi) / 2;
+    const k = 3 * alas;
+    console.log("Luas Segitiga:", luas, "cm^2");
+    console.log("Keliling Segitiga:", keliling, "cm");
+    // TODO: gunakan console.log untuk mencetak hasil luas dan keliling
+    
+    // Akhir pembuatan code
+  }
+  
+  TentukanLuasDanKelilingSegitigaSamaSisi(1, 2);
